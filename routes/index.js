dfd0acbb67b81060b2d1fdc742339bf64165f1a3
Francis Devereux <francis@bright-interactive.co.uk>
2015-09-11 10:31:44 +0200

    Read from MongoDB

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,15 @@ router.get('/helloworld', function(req, res) {
   res.render('helloworld', { title: 'Hello, World!' });
 });
 
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({},{},function(e,docs){
+    res.render('userlist', {
+      'title': 'User List',
+      'userlist': docs
+    });
+  });
+});
+
 module.exports = router;
